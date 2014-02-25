8473c7e85f3135ee40133f5d1a208d04bb2b9e85
christian <callowaylc@gmail.com>
2014-02-25 00:03:08 +0000

    updating cap deploy files

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,70 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+#=============================================================================
+# app details and WordPress requirements
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+# tags/3.5.1, branches/3.5, trunk
+set :application, "geekshut"
 
-# Default branch is :master
+#=============================================================================
+# app source repository configuration
+
+set :scm, :git
+set :repo_url, "git@github.com:callowaylc/gw-geekshut.git"
+#set :git_enable_submodules, 1
+#set :git_shallow_clone, 1
+
+#=============================================================================
+# Housekeeping
+# clean up old releases on each deploy
+set :keep_releases, 5
+#set :ssh_options, { :forward_agent => true }
+
+ # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/ubuntu/Develop/gw-geekshut'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm,    :git
 
-# Default value for :format is :pretty
-# set :format, :pretty
+# set server user
+set :user,   "ubuntu"
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+# deployment will be done via copy
+set :deploy_via, :remote_cache
 
-# Default value for :pty is false
-# set :pty, true
+# only keep the last 5 releases
+set :keep_releases, 5
 
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+#=============================================================================
+# Additional Project specific directories
 
 namespace :deploy do
 
   desc 'Restart application'
+  desc 'Kick varnish'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+    on roles(:web), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
+
+      # restart unicorn server
+      # desc 'kicking varnish tires'
+      execute 'sudo service nginx restart'
+      execute 'sudo service varnish restart'
     end
   end
+
+  desc "Check that we can access everything"
+  task :check_write_permissions do
+    on roles(:all) do |host|
+      if test("[ -w #{fetch(:deploy_to)} ]")
+        info "#{fetch(:deploy_to)} is writable on #{host}"
+      else
+        error "#{fetch(:deploy_to)} is not writable on #{host}"
+      end
+    end
+  end  
 
   after :publishing, :restart
 
@@ -56,3 +78,15 @@ namespace :deploy do
   end
 
 end
+
+# Uncomment these lines to additionally create your upload and cache
+# directories in the shared location when running `deploy:setup`.
+#
+# Modify these commands to make sure these directories are writable by
+# your web server.
+
+# after "deploy:setup" do
+#   ['uploads', 'cache'].each do |dir|
+#     run "cd #{shared_path} && mkdir #{dir} && chgrp www-data #{dir} && chmod 775 #{dir}"
+#   end
+# end
